7f6974db295f7ca08cfc13f3ee9c8f76957112e1
Bluecoast <51225257+Bluecoast-Heiko@users.noreply.github.com>
2019-10-11 15:09:16 +1000

    new upload

---
+++ pointclouds/JSPW_July2019/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=56 +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
+    "points": 15970389,
+    "boundingBox": {
+        "lx": 559641.76,
+        "ly": 6831587.100000001,
+        "lz": -8.74,
+        "ux": 560151.97,
+        "uy": 6832097.3100000009,
+        "uz": 501.46999999996276
+    },
+    "tightBoundingBox": {
+        "lx": 559641.74,
+        "ly": 6831587.11,
+        "lz": -8.75,
+        "ux": 560151.96,
+        "uy": 6831802.62,
+        "uz": 15.31
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 4.418548107147217,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
