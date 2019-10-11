1e022def31337a9da853425704c1aebc15fc6bf6
Bluecoast <51225257+Bluecoast-Heiko@users.noreply.github.com>
2019-10-11 11:51:11 +1000

    Initial upload

---
+++ pointclouds/The Pass to Belongil July 2019/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm",
+    "points": 90170083,
+    "boundingBox": {
+        "lx": 559545.33,
+        "ly": 6831307.390000001,
+        "lz": -8.74,
+        "ux": 561703.46,
+        "uy": 6833465.5200000009,
+        "uz": 2149.390000000005
+    },
+    "tightBoundingBox": {
+        "lx": 559545.33,
+        "ly": 6831307.38,
+        "lz": -8.75,
+        "ux": 561703.47,
+        "uy": 6832173.25,
+        "uz": 42.53
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 18.68995475769043,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
